fc1091c0faf9c3f42b1da00c2a992a9ef83e8217
newberg85 <bergviana12345@gmail.com>
2024-12-15 16:08:14 -0300

    test: erro .env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,11 +34,11 @@ export default function Home() {
   useEffect(() => {
     const getInitialData = async () => {
       setLoading(true);
-      try {
-        const apikey = process.env.NEXT_PUBLIC_API_KEY;
-        console.log("Api Key:", apikey);
+      // try {
+      //   const apikey = process.env.NEXT_PUBLIC_API_KEY;
+      //   console.log("Api Key:", apikey);
         const response = await fetch(
-          `https://geo.ipify.org/api/v2/country,city?apiKey=${apikey}`
+          `https://geo.ipify.org/api/v2/country,city?apiKey=at_aqInlQYid2YfCg6dSKfCtwg2TlUQ0`
         );
         const data = await response.json();
         setAddress(data);
@@ -56,10 +56,10 @@ export default function Home() {
     event.preventDefault();
     setLoading(true);
     try {
-      const apikey = process.env.NEXT_PUBLIC_API_KEY;
-      console.log("Api Key:", apikey);
+      // const apikey = process.env.NEXT_PUBLIC_API_KEY;
+      // console.log("Api Key:", apikey);
       const response = await fetch(
-        `https://geo.ipify.org/api/v2/country,city?apiKey=${apikey}&ipAddress=${ipAddress}`
+        `https://geo.ipify.org/api/v2/country,city?apiKey=at_aqInlQYid2YfCg6dSKfCtwg2TlUQ0&ipAddress=${ipAddress}`
       );
       const data = await response.json();
       setAddress(data);
